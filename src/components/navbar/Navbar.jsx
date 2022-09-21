020ef0ee72e70d4672f1cd2e069cbaf8a9a18790
elmaaroufi1 <abdessamad.maaroufi12@gmail.com>
2022-09-21 09:04:46 +0100

    last update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,55 +1,24 @@
 import './navbar.css'
-import { useState } from 'react';
-import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 import logo from '../../assets/logo.png'
-// BEM Block Element Modifier
-
-const Menu = ()=>{
-  return <>
-  <p><a href="#home">Home</a></p>
-  <p><a href="#wgpt3">What is GPT3</a></p>
-  <p><a href="#possibility">Open AI</a></p>
-  <p><a href="#features">Case Studies</a></p>
-  <p><a href="#blog">Library</a></p>
-  </>
-}
+import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
-  const [toggleMenu, setToggleMenu] = useState(false)
+  const navigate = useNavigate()
   return (
-    <div className='gpt3__navbar'>
-      <div className='gpt3__navbar-links'>
-         <div className='gpt3__navbar-links_logo'>
-           <img src={logo} alt="logo" />
-         </div>
-         <div className='gpt3__navbar-links_container'>
-            <Menu/>
-         </div>
+    <nav>
+      <div onClick={()=>navigate('/')} className="logo">
+        <img src={logo} alt="" />
       </div>
-      <div className='gpt3__navbar-sign'>
-          <p>Sign In</p>
-          <button type='button'>Sign Up</button>
+
+      <div className="navbar">
+        <ul>
+         <li><a href="#about"><span>01.</span> About</a></li>
+         <li><a href="#projects"><span>02.</span>Projects</a></li>
+         <li><Link  to='/digital' ><a href="#work"><span>03.</span> Work</a></Link></li>
+         <li><a href="#contact"><span>04.</span> Contact</a></li>
+        </ul>
       </div>
-      <div className='gpt3__navbar-menu'>
-           {toggleMenu 
-             ? <RiCloseLine color='#fff' size={27} onClick = {()=>setToggleMenu(false)} /> 
-             : <RiMenu3Line color='#fff' size={27} onClick = {()=>setToggleMenu(true)} /> 
-          }
-          {
-            toggleMenu
-            && (
-              <div className='gpt3__navbar-menu_container scale-up-center'>
-                <div className='gpt3__navbar-menu_container-links'>
-                    <Menu/>
-                    <div className='gpt3__navbar-menu_container-links-sign'>
-                      <p>Sign In</p>
-                      <button type='button'>Sign Up</button>
-                    </div>
-                </div>
-              </div>
-            )
-          }
-      </div>
-    </div>
+    </nav>
   )
 }
 
